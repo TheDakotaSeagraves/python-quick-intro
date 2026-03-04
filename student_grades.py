da11ac8591dd111106e0e233f187c5e75d9e3914
Dakota Seagraves <dakotaseagraves31@gmail.com>
2026-03-04 14:54:45 -0600

    Add files via upload

---
+++ student_grades.py
@@ -0,0 +1,40 @@
+student_grades = {}
+
+
+# Function to add a student
+def add_student(name, grade):
+    student_grades[name] = grade
+    print(f"Student `{name}` added with grade {grade}.")
+
+# Function to remove a student
+def remove_student(name):
+    if name in student_grades:
+        del student_grades[name]
+        print(f"Student `{name}` removed.")
+    else:
+        print(f"Student `{name}` not found.")
+
+# Function to display all students and grades
+def display_Student():
+    print("Student:")
+    for name, grade in student_grades.items():
+        print(f"{name}: {grade}")
+
+# adding students
+add_student("Dakota","C" )
+add_student("Maggie", "A")
+add_student("Kati", "A")
+add_student("Magnus", "C")
+add_student("Kayla", "B")
+add_student("Damean", "D")
+print("-----------------------")
+#Displaying students and grades
+display_Student()
+print("-----------------------")
+#Removing a student
+remove_student("Damean")
+print("-----------------------")
+#Displaying students again
+display_Student()
+print("-----------------------")
+print(student_grades)
